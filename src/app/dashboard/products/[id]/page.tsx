e07332a7a1293b8badb0d82040606ad4310a28a7
VictorDev <ememevictor08@gmail.com>
2026-04-06 16:39:46 +0100

    done with products 2

--- src/app/dashboard/products/[id]/page.tsx
+++ src/app/dashboard/products/[id]/page.tsx
@@ -15,7 +15,7 @@ export default async function ProductViewPage({ params }: Readonly<Props>) {
   const { id } = await params;
 
   return (
-    <main className="p-6 max-w-6xl mx-auto">
+    <main className="p-6 max-w-6xl mx-auto mb-14">
       <ProductsPageClient storeId={id} />
     </main>
   );
