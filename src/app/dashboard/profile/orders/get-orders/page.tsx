ee05934816c7b22fddea22e75bdce48e5cf47326
VictorDev <ememevictor08@gmail.com>
2026-04-10 14:37:28 +0100

    meta fix

--- src/app/dashboard/profile/orders/get-orders/page.tsx
+++ src/app/dashboard/profile/orders/get-orders/page.tsx
@@ -1,10 +1,6 @@
 // src/app/dashboard/profile/orders/get-orders/page.tsx
 import GetOrdersComponent from "@/components/orders/get-orders";
-import type { Metadata } from "next";
 
-export const metadata: Metadata = {
-    title: "Orders",
-};
 
 export default function GetOrdersPage() {
     return <GetOrdersComponent />
